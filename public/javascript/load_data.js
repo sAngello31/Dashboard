c898a3d77e0e6e6391b525a2e266a3605e1bdb7e
Angello de Jesus Bravo Mera <bravoangello31@gmail.com>
2023-11-13 07:21:50 -0500

    cambio de apis y validaciones

--- public/javascript/load_data.js
+++ public/javascript/load_data.js
@@ -1,7 +1,12 @@
+import {ciudadesEcuador} from './static_data.js';
 
 let fechaActual = () => new Date().toISOString().slice(0,10);
 let tiempoArr = [];
+let chart1 = null;
+let chart2 = null
+let arrChart = [chart1, chart2];
 
+// Aqui inicia los promedios y gráficos
 let filtrar = (arregloDatos) => {
     let datos = [];
     let fecha = fechaActual();
@@ -25,36 +30,93 @@ let sacarPromedios = (arrDatos) => {
     return datos;
 }
 
-let cargarOpenMeteor = () => {
-    let URL = "https://api.open-meteo.com/v1/forecast?latitude=-2.1962&longitude=-79.8862&current=precipitation&hourly=temperature_2m,precipitation,windspeed_10m&timezone=auto";
-    fetch(URL).then(responseText => responseText.json()).then(responseJSON => {
-        let plotRef = document.getElementById("plot1");
-        let labels1 = responseJSON.hourly.time;
-        let data1 = responseJSON.hourly.temperature_2m;
-        let config1 = {
-            type: 'line',
-            data: {
-              labels: labels1, 
-              datasets: [
-                {
-                  label: 'Temperature [2m]',
-                  data: data1, 
-                }
-              ]
-            }
-        };
-        tiempoArr = responseJSON.hourly.time
-        cargarTemperatura(responseJSON.hourly.temperature_2m);
-        cargarViento(responseJSON.hourly.windspeed_10m);
-        cargarPrecipitacion(responseJSON.hourly.precipitation);
+let validarChart = () => {
+    for(let i in arrChart){
+        if(arrChart[i] != null){
+            arrChart[i].destroy();
+        }
+    }
+}
 
-
-
-        let chart1  = new Chart(plotRef, config1);
-    }).catch(console.error);
+let llenarCiudades = () =>{
+    let menu = document.getElementById("opcionesCiudades");
+    for(let clave in ciudadesEcuador){
+        let option = document.createElement("option");
+        option.textContent = clave;
+        option.value = clave;
+        menu.appendChild(option);
+    }
 }
 
 
+let cargarFechaActual = () => {
+    let coleccionHTML = document.getElementsByTagName("h6");
+    let tituloH6 = coleccionHTML[0];
+    tituloH6.textContent = fechaActual();
+}
+
+let makeChart = (data) => {
+        let plotRef = document.getElementById("plot1");
+        let plotRef2 = document.getElementById("plot2");
+        let config1 = {
+            type: 'line',
+            data: {
+              labels: data.hourly.time, 
+              datasets: [
+                {
+                  label: 'Temperature [2m]',
+                  data: data.hourly.temperature_2m, 
+                }
+              ]
+            },
+            options: {
+                responsive: true
+            }
+        };
+
+        let config2 = {
+            type: 'bar',
+            data: {
+              labels: data.daily.time, 
+              datasets: [
+                {
+                  label: 'Índices UV Semanales',
+                  data: data.daily.uv_index_max, 
+                }
+              ]
+            },
+            options: {
+                responsive: true
+            }
+        }
+
+        arrChart[0]  = new Chart(plotRef, config1);
+        arrChart[1] = new Chart(plotRef2, config2);
+}
+
+let cargarOpenMeteor = (urlCiudad) => {
+    validarChart();
+    fetch(urlCiudad).then(responseText => responseText.json()).then(responseJSON => {
+        
+        let latitud = document.getElementById("latitud");
+        let longitud = document.getElementById("longitud");
+        let elevacion = document.getElementById("elevacion");
+
+        elevacion.textContent = responseJSON.elevation;
+        latitud.textContent = responseJSON.latitude;
+        longitud.textContent = responseJSON.longitude;
+
+
+        makeChart(responseJSON);
+        tiempoArr = responseJSON.hourly.time
+        cargarTemperatura(responseJSON.hourly.temperature_2m);
+        cargarViento(responseJSON.hourly.wind_speed_10m);
+        cargarPrecipitacion(responseJSON.hourly.relative_humidity_2m);
+        
+    }).catch(console.error);
+}
+
+//Ahora precipitacion es para referirse a humedad xd
 let cargarPrecipitacion = (arrJSONDatos) => {
     let datosPrecipitaciones = filtrar(arrJSONDatos);
     let mediciones = sacarPromedios(datosPrecipitaciones);
@@ -63,9 +125,9 @@ let cargarPrecipitacion = (arrJSONDatos) => {
     let precipitacionPromValue = document.getElementById("precipitacionPromValue");
     let precipitacionMaxValue = document.getElementById("precipitacionMaxValue");
 
-    precipitacionMinValue.textContent = `Min ${mediciones[0]} [mm]`;
-    precipitacionPromValue.textContent = `Prom ${Math.round(mediciones[1]*100) / 100} [mm]`;
-    precipitacionMaxValue.textContent = `Max ${mediciones[2]} [mm]`;
+    precipitacionMinValue.textContent = `Min ${mediciones[0]} [%]`;
+    precipitacionPromValue.textContent = `Prom ${Math.round(mediciones[1]*100) / 100} [%]`;
+    precipitacionMaxValue.textContent = `Max ${mediciones[2]} [%]`;
 }
 
 let cargarViento = (arrJSONDatos) => {
@@ -94,13 +156,87 @@ let cargarTemperatura = (arrJSONDatos) => {
     temperaturaMaxValue.textContent =  `Max ${mediciones[2]} [°C]`;
 }
 
-let cargarFechaActual = () => {
-    let coleccionHTML = document.getElementsByTagName("h6");
-    let tituloH6 = coleccionHTML[0];
-    tituloH6.textContent = fechaActual();
+
+
+//Aqui acaba los promedioss
+
+let parseXML = (responseText) => {
+  
+    // Parsing XML
+    const parser = new DOMParser();
+    const xml = parser.parseFromString(responseText, "application/xml");
+
+    let forecastElement = document.querySelector("#forecastbody")
+    let ciudad = document.getElementById("coordenadas");
+    
+
+    forecastElement.innerHTML = ''
+
+    // Procesamiento de los elementos con etiqueta `<time>` del objeto xml
+    let timeArr = xml.querySelectorAll("time")
+
+    timeArr.forEach(time => {
+        
+        let from = time.getAttribute("from").replace("T", " ")
+
+        let humidity = time.querySelector("humidity").getAttribute("value")
+        let windSpeed = time.querySelector("windSpeed").getAttribute("mps");
+        let precipitation = time.querySelector("precipitation ").getAttribute("probability");
+        let pressure = time.querySelector("pressure").getAttribute("value");
+        let cloud = time.querySelector("clouds").getAttribute("all");
+
+        let template = `
+            <tr>
+                <td>${from}</td>
+                <td>${humidity}</td>
+                <td>${windSpeed}</td>
+                <td>${precipitation}</td>
+                <td>${pressure}</td>
+                <td>${cloud}</td>
+            </tr>
+        `
+
+        //Renderizando la plantilla en el elemento HTML
+        forecastElement.innerHTML += template;
+    })
 }
 
 
+//Callback
+let selectListener = async (event) => {
+    let selectedCity = event.target.value
+    let cityStorage = localStorage.getItem(selectedCity);
+
+    if(cityStorage == null){
+        try {
+
+            //API key
+            let APIkey = 'f1fb8311364a9975b722abe6aa59787d';
+            let url = `https://api.openweathermap.org/data/2.5/forecast?q=${selectedCity}&mode=xml&appid=${APIkey}`
+            
+            let response = await fetch(url);
+            let responseText = await response.text();
+            await cargarOpenMeteor(ciudadesEcuador[selectedCity]);
+            await parseXML(responseText);
+            await localStorage.setItem(selectedCity, responseText);
+    
+        } catch (error) {
+            console.log(error)
+        }
+    } else{
+        parseXML(cityStorage);
+        cargarOpenMeteor(ciudadesEcuador[selectedCity]); //CORREGIR ESTO
+    }
+}
+
+let loadForecastByCity = () => {
+    //Handling event
+    let selectElement = document.querySelector("select");
+    selectElement.addEventListener("change", selectListener);
+  
+}
+
+llenarCiudades();
 cargarFechaActual();
-cargarOpenMeteor();
+loadForecastByCity();
 
--- public/javascript/static_data.js
+++ public/javascript/static_data.js
@@ -678,3 +678,18 @@ export let temperaturaArr = [
     10.5,
     10.3
 ]
+
+export let ciudadesEcuador = {
+    "Quito": "https://api.open-meteo.com/v1/forecast?latitude=-0.2298&longitude=-78.525&current=temperature_2m&hourly=temperature_2m,relative_humidity_2m,wind_speed_10m&daily=uv_index_max&timezone=auto",
+    "Guayaquil": "https://api.open-meteo.com/v1/forecast?latitude=-2.1962&longitude=-79.8862&current=temperature_2m&hourly=temperature_2m,relative_humidity_2m,wind_speed_10m&daily=uv_index_max&timezone=auto",
+    "Cuenca": "https://api.open-meteo.com/v1/forecast?latitude=-2.9005&longitude=-79.0045&current=temperature_2m&hourly=temperature_2m,relative_humidity_2m,wind_speed_10m&daily=uv_index_max&timezone=auto",
+    "Manta": "https://api.open-meteo.com/v1/forecast?latitude=-0.9621&longitude=-80.7127&current=temperature_2m&hourly=temperature_2m,relative_humidity_2m,wind_speed_10m&daily=uv_index_max&timezone=auto",
+    "Ambato": "https://api.open-meteo.com/v1/forecast?latitude=-1.2491&longitude=-78.6167&current=temperature_2m&hourly=temperature_2m,relative_humidity_2m,wind_speed_10m&daily=uv_index_max&timezone=auto",
+    "Loja": "https://api.open-meteo.com/v1/forecast?latitude=-3.9931&longitude=-79.2042&current=temperature_2m&hourly=temperature_2m,relative_humidity_2m,wind_speed_10m&daily=uv_index_max&timezone=auto",
+    "Ibarra": "https://api.open-meteo.com/v1/forecast?latitude=0.3517&longitude=-78.1223&current=temperature_2m&hourly=temperature_2m,relative_humidity_2m,wind_speed_10m&daily=uv_index_max&timezone=auto",
+    "Riobamba": "https://api.open-meteo.com/v1/forecast?latitude=-1.671&longitude=-78.6471&current=temperature_2m&hourly=temperature_2m,relative_humidity_2m,wind_speed_10m&daily=uv_index_max&timezone=auto",
+    "Portoviejo": "https://api.open-meteo.com/v1/forecast?latitude=-1.0546&longitude=-80.4544&current=temperature_2m&hourly=temperature_2m,relative_humidity_2m,wind_speed_10m&daily=uv_index_max&timezone=auto",
+    "Machala": "https://api.open-meteo.com/v1/forecast?latitude=-3.2586&longitude=-79.9605&current=temperature_2m&hourly=temperature_2m,relative_humidity_2m,wind_speed_10m&daily=uv_index_max&timezone=auto",
+    "Santa Elena": "https://api.open-meteo.com/v1/forecast?latitude=-2.2262&longitude=-80.8587&current=temperature_2m&hourly=temperature_2m,relative_humidity_2m,wind_speed_10m&daily=uv_index_max&timezone=auto"
+  };
+  
